6885033418d956f83709d6dcdce5661a76924b99
DylanLennard <dlennard09@yahoo.com>
2016-12-23 07:33:04 -0800

    First commit having done the lesson

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,15 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+// .append is using jquery
+//# means looking for element with id of "main"
+//$("#main").append("Dylan Lennard");
+
+var email = "dylanjlennard@yahoo.com";
+var newEmail = email.replace("yahoo", "google");
+console.log(email);
+console.log(newEmail);
+
+
+var awesomeThoughts = "I am Dylan and I am AWESOME";
+console.log(awesomeThoughts);
+
+var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
+$("#main").append(funThoughts);
