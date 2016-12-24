6625579fb8a83372a94cf51fa210381ee281f264
DylanLennard <dlennard09@yahoo.com>
2016-12-23 21:09:51 -0800

    Added comments on functionality regarding strings in js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -11,5 +11,21 @@ console.log(newEmail);
 var awesomeThoughts = "I am Dylan and I am AWESOME";
 console.log(awesomeThoughts);
 
-var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
-$("#main").append(funThoughts);
+//var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
+//$("#main").append(funThoughts);
+
+
+//mixing .replace and .append() to replace
+var name = "Dylan Lennard"
+var role = "Associate Product Manager and Product Analyst"
+var formattedName = HTMLheaderName.replace("%data%", name);
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+$("#header").prepend(formattedName);
+$("#header").prepend(formattedRole);
+
+//playing with strings
+//s = audacity
+// s = s[1].toUpperCase + s.slice(2) will turn it to Udacity!
+
+///arrays in js are just like lists
